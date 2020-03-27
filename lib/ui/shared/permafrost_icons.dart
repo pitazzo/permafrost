8b76c87871b81e6461ae3c2f5be49faa834b5d9f
pitazzo <pedromalo@live.com>
2020-03-27 17:32:34 +0100

    optimise: remove unused icons

--- lib/ui/shared/permafrost_icons.dart
+++ lib/ui/shared/permafrost_icons.dart
@@ -5,25 +5,22 @@ class PermafrostIcon {
 
   static const _kFontFam = 'PermafrostIcon';
 
-  static const IconData snack = const IconData(0xe800, fontFamily: _kFontFam);
-  static const IconData freezer = const IconData(0xe801, fontFamily: _kFontFam);
-  static const IconData shrimp = const IconData(0xe802, fontFamily: _kFontFam);
-  static const IconData cheese = const IconData(0xe803, fontFamily: _kFontFam);
-  static const IconData drinks = const IconData(0xe804, fontFamily: _kFontFam);
-  static const IconData ice = const IconData(0xe805, fontFamily: _kFontFam);
-  static const IconData tuppers = const IconData(0xe806, fontFamily: _kFontFam);
-  static const IconData fish = const IconData(0xe807, fontFamily: _kFontFam);
-  static const IconData misc = const IconData(0xe808, fontFamily: _kFontFam);
-  static const IconData greens = const IconData(0xe809, fontFamily: _kFontFam);
-  static const IconData meat = const IconData(0xe80a, fontFamily: _kFontFam);
-  static const IconData bread = const IconData(0xe80b, fontFamily: _kFontFam);
-  static const IconData fruits = const IconData(0xe80c, fontFamily: _kFontFam);
-  static const IconData icecream = const IconData(0xe80d, fontFamily: _kFontFam);
-  static const IconData fridge = const IconData(0xe80e, fontFamily: _kFontFam);
+  static const IconData snack = IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData shrimp = IconData(0xe802, fontFamily: _kFontFam);
+  static const IconData cheese = IconData(0xe803, fontFamily: _kFontFam);
+  static const IconData drinks = IconData(0xe804, fontFamily: _kFontFam);
+  static const IconData ice = IconData(0xe805, fontFamily: _kFontFam);
+  static const IconData tuppers = IconData(0xe806, fontFamily: _kFontFam);
+  static const IconData fish = IconData(0xe807, fontFamily: _kFontFam);
+  static const IconData misc = IconData(0xe808, fontFamily: _kFontFam);
+  static const IconData greens = IconData(0xe809, fontFamily: _kFontFam);
+  static const IconData meat = IconData(0xe80a, fontFamily: _kFontFam);
+  static const IconData bread = IconData(0xe80b, fontFamily: _kFontFam);
+  static const IconData fruits = IconData(0xe80c, fontFamily: _kFontFam);
+  static const IconData icecream = IconData(0xe80d, fontFamily: _kFontFam);
 
   static const Map<String, IconData> IconsMap = <String, IconData>{
     'snack' : snack,
-    'freezer' : freezer,
     'shrimp' : shrimp,
     'cheese' : cheese,
     'drinks' : drinks,
@@ -36,6 +33,5 @@ class PermafrostIcon {
     'bread' : bread,
     'fruits' : fruits,
     'icecream' : icecream,
-    'fridge' : fridge
   };
 }
